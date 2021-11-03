8b4cc09b88afdc532eb9e02f2d7f75bd9fb780c2
Simon <64868759+Sim0nSB@users.noreply.github.com>
2021-11-03 09:18:43 +0000

    Aufgabe 1 done

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+int aufgabe1(), aufgabe2(), aufgabe3(), aufgabe4(), aufgabe5(), aufgabe6();
 
 int main(void) {
+
+  aufgabe1();
+  //aufgabe2();
+  //aufgabe3();
+  //aufgabe4();
+  //aufgabe5();
+  //aufgabe6();
   
-  
+
+}
+
+//Aufgabe 1
+int aufgabe1() {
+    int eingabe = 0, ausgabe = 0;
+    scanf("%d", & eingabe);
+    if (eingabe >= 0) {
+      for (int i = 1; i <= eingabe; i++) {
+        ausgabe += i;
+      }
+      printf("%d", ausgabe);
+    } else {
+      printf("Eine Natürliche Zahl eingeben");
+    }
   return 0;
 }
